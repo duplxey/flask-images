2136dde7ae9fc6600bdea6c71f955b5bde6895f0
Nik Tomazic <nick.tomazic@gmail.com>
2022-03-02 17:55:10 +0100

    connected the app with the database

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import os
-
-from flask import Flask
+import sqlite3
+from flask import Flask, send_from_directory
 from flask import redirect
 from flask import render_template
 from flask import request
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = 'media/'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = 'x&7znvo5yo!)04p62qiicm2o_dt560wdt+$q_7t+(94w$c*zhg'
+app.config['UPLOAD_FOLDER'] = 'media/'
 
 
 def allowed_file(filename):
@@ -18,36 +18,49 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+@app.route('/media/<path:path>')
+def media(path):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], path)
+
+
 @app.route('/')
 def index():
-    images = ['one', 'two', 'three']
+    conn = get_db_connection()
+    images = conn.execute('SELECT * FROM images ORDER BY uploaded_at DESC').fetchall()
+    conn.close()
     return render_template('index.html', images=images)
 
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    if 'file' not in request.files or 'title' not in request.form:
-        app.logger.warning('Upload failed. No file or title was provided.')
-        return redirect('/', code=302)
-
     title = request.form['title']
     file = request.files['file']
 
-    if title == '':
-        app.logger.warning('No title was provided.')
-        return redirect('/', code=302)
-
-    if file.filename == '':
-        app.logger.warning('No file was provided.')
+    if not title or not file or title == '' or file.filename == '':
+        app.logger.warning('No title or file were provided.')
         return redirect('/', code=302)
 
     if not allowed_file(file.filename):
         app.logger.warning('Invalid file format. Allowed formats: ' + ', '.join(ALLOWED_EXTENSIONS) + '.')
         return redirect('/', code=302)
 
+    # save the file to the UPLOAD_FOLDER
     filename = secure_filename(file.filename)
     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    app.logger.info(f'File has been successfully uploaded: {filename}.')
+
+    # add an entry to the database
+    conn = get_db_connection()
+    conn.execute('INSERT INTO images(title, file) VALUES (?, ?)', (title, filename))
+    conn.commit()
+    conn.close()
+
+    app.logger.info(f'File {filename} has been successfully uploaded.')
     return redirect('/', code=302)
 
 
