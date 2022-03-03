0bd51e1e04f1a93f96d4f2e954e71a48a9bd35a9
Nik Tomazic <nick.tomazic@gmail.com>
2022-03-03 01:03:38 +0100

    code clean up

--- app.py
+++ app.py
@@ -6,16 +6,14 @@ from flask import render_template
 from flask import request
 from werkzeug.utils import secure_filename
 
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'x&7znvo5yo!)04p62qiicm2o_dt560wdt+$q_7t+(94w$c*zhg'
+app.config['UPLOAD_ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
 app.config['UPLOAD_FOLDER'] = 'media/'
 
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['UPLOAD_ALLOWED_EXTENSIONS']
 
 
 def get_db_connection():
@@ -24,17 +22,23 @@ def get_db_connection():
     return conn
 
 
-@app.route('/media/<path:path>')
-def media(path):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], path)
+def get_db_images():
+    conn = get_db_connection()
+    results = conn.execute('SELECT * FROM images ORDER BY uploaded_at DESC').fetchall()
+    conn.close()
+    return results
+
+
+def insert_db_image(title, filename):
+    conn = get_db_connection()
+    conn.execute('INSERT INTO images(title, file) VALUES (?, ?)', (title, filename))
+    conn.commit()
+    conn.close()
 
 
 @app.route('/')
 def index():
-    conn = get_db_connection()
-    images = conn.execute('SELECT * FROM images ORDER BY uploaded_at DESC').fetchall()
-    conn.close()
-    return render_template('index.html', images=images)
+    return render_template('index.html', images=get_db_images())
 
 
 @app.route('/upload', methods=['POST'])
@@ -47,21 +51,25 @@ def upload():
         return redirect('/', code=302)
 
     if not allowed_file(file.filename):
-        app.logger.warning('Invalid file format. Allowed formats: ' + ', '.join(ALLOWED_EXTENSIONS) + '.')
+        app.logger.warning('Invalid file format. Allowed formats: png, jpg, jpeg, gif.')
         return redirect('/', code=302)
 
-    # save the file to the UPLOAD_FOLDER
     filename = secure_filename(file.filename)
     file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-
-    # add an entry to the database
-    conn = get_db_connection()
-    conn.execute('INSERT INTO images(title, file) VALUES (?, ?)', (title, filename))
-    conn.commit()
-    conn.close()
+    insert_db_image(title, filename)
 
     app.logger.info(f'File {filename} has been successfully uploaded.')
     return redirect('/', code=302)
+
+
+@app.route('/media/<path:path>')
+def media(path):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], path)
+
+
+@app.template_global()
+def media(filename):
+    return os.path.join(app.config['UPLOAD_FOLDER'], filename)
 
 
 if __name__ == '__main__':
